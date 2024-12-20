0531890e74f6cf3ff66deb289eb7beae7c030558
Hao Wang <13921265345@163.com>
2024-12-20 06:48:40 +0000

    Complete Logging System

--- ansible/total_setup.sh
+++ ansible/total_setup.sh
@@ -1,36 +1,78 @@
 #!/bin/bash
 
-
+# Set environment to local
 export ENVIRONMENT=local
 
-
-run_command() {
-  echo "Run: $1"
-  eval $1
-  if [ $? -ne 0 ]; then
-    echo "Fail: $1"
-    exit 1
-  fi
+# Function to check the success of the last command
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo "Error: The previous command failed. Exiting."
+        exit 1
+    fi
 }
 
+# Run Ansible playbooks in sequence
+echo "Running setup.yml"
+sudo ansible-playbook -i environments/local setup.yml
+check_success
 
-run_command "sudo ansible-playbook -i environments/\$ENVIRONMENT openwhisk.yml -e mode=clean"
-run_command "sudo ansible-playbook -i environments/local setup.yml"
-run_command "sudo systemctl stop couchdb"
+echo "Running prereq.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT prereq.yml
+check_success
 
+# Stop services
+echo "Stopping CouchDB"
+sudo systemctl stop couchdb
+check_success
 
-run_command "cd .. && ./gradlew distDocker && cd ansible"
+echo "Stopping etcd"
+sudo systemctl stop etcd
+check_success
 
+# Go back to previous directory
+cd ..
+check_success
 
-run_command "sudo ansible-playbook -i environments/\$ENVIRONMENT couchdb.yml"
+# Build Docker image
+echo "Building Docker image"
+./gradlew distDocker
+check_success
 
+# Go back to ansible directory
+cd ansible
+check_success
 
-run_command "sudo ansible-playbook -i environments/\$ENVIRONMENT initdb.yml"
+# Run Ansible playbooks
+echo "Running couchdb.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT couchdb.yml
+check_success
 
+echo "Running initdb.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT initdb.yml
+check_success
 
-run_command "sudo ansible-playbook -i environments/\$ENVIRONMENT wipe.yml"
+echo "Running wipe.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT wipe.yml
+check_success
 
+echo "Running elasticsearch.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT elasticsearch.yml
+check_success
 
-run_command "sudo ansible-playbook -i environments/\$ENVIRONMENT openwhisk.yml"
+echo "Running openwhisk.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT openwhisk.yml
+check_success
 
-echo "All Works Done！"
+echo "Running postdeploy.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT postdeploy.yml
+check_success
+
+echo "Running apigateway.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT apigateway.yml
+check_success
+
+echo "Running routemgmt.yml"
+sudo ansible-playbook -i environments/$ENVIRONMENT routemgmt.yml
+check_success
+
+echo "Deployment complete!"
